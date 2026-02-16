4a301d0f2f0089089a43e8a5a1c96deefc6c0ebe
groovewaves-prog <groovewaves@gmail.com>
2026-02-16 16:58:03 +0900

    Create init_session_state function for session management
    Initialize session state with default values for various parameters.

---
+++ utils/state.py
@@ -0,0 +1,30 @@
+# utils/state.py
+import streamlit as st
+
+def init_session_state():
+    """セッション状態を初期化"""
+    defaults = {
+        "site_scenarios": {},
+        "active_site": None,
+        "maint_flags": {},
+        "live_result": None,
+        "verification_result": None,
+        "generated_report": None,
+        "remediation_plan": None,
+        "verification_log": None,
+        "messages": [],
+        "chat_session": None,
+        "chat_quick_text": "",
+        "trigger_analysis": False,
+        "logic_engines": {},
+        "balloons_shown": False,
+        "recovered_devices": {},
+        "recovered_scenario_map": {},
+        "report_cache": {},
+        "injected_weak_signal": None,
+        "tuning_report": None, # v45.0
+    }
+    
+    for key, default in defaults.items():
+        if key not in st.session_state:
+            st.session_state[key] = default
